981d93e81d01fd6ef0dfa5635f3ee9894bb1b206
ayo oke <haywee39@gmail.com>
2022-10-27 05:00:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("You are about to view my profile flipcard.\nKindly hover on card to flip to the backside\nfor more info about me.\nThanks.\nTAGS-Techie.\nAyoOke-2022 ")
+   
+function myportfolio(){
+    var flight = document.getElementsByTagName("btn").innerHTML="Portfolio";
+    alert("Off to my web portfolio: ayo-oke.netlify.app\n Please click 'OK' to proceed")
+}
